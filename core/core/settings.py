cd5f8fde9630f36e905aa3c7b2155d7e9420a57b
farzan-alaei <farzan.70@gmail.com>
2023-04-08 18:01:19 +0330

    get_user_model method to models of blog app
    add get_user_model method to models.py of blog app and config post and category model to admin.py of blog app

--- core/core/settings.py
+++ core/core/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    # 'blog',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
-MEDIA_URL = 'media'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 
