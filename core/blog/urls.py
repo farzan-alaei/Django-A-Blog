b235f2bdd4f34c872841955bd9103b459e994483
farzan-alaei <farzan.70@gmail.com>
2023-04-09 15:01:06 +0330

    add class indexview in views of blog app
    add class IndexView to blog app for view the class in index.html and test the template view . add posts in index.html
    . add url path for IndexView in blog app

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import indexView
+from . import views
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('fbv-index', indexView, name='fbv-index'),
-    path('cbv-index', TemplateView.as_view(template_name='index.html', extra_context={"name": "ali"}))
+    path('fbv-index', views.indexView, name='fbv-index'),
+    # path('cbv-index', TemplateView.as_view(template_name='index.html', extra_context={"name": "ali"}))
+    path('cbv-index', views.IndexView.as_view(), name='cbv-index'),
 ]
