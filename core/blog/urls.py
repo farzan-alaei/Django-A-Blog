8c0a7938185d390aba6a56d0977324ad8c195cd2
farzan-alaei <farzan.70@gmail.com>
2023-04-09 17:24:15 +0330

    add redirect view to urls.py in blog app
    add RedirectView in urls.py in blog app for redirection urls in web

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 from . import views
-from django.views.generic import TemplateView
+from django.views.generic.base import TemplateView, RedirectView
+
+app_name = "blog"
+
 
 urlpatterns = [
     path('fbv-index', views.indexView, name='fbv-index'),
     # path('cbv-index', TemplateView.as_view(template_name='index.html', extra_context={"name": "ali"}))
     path('cbv-index', views.IndexView.as_view(), name='cbv-index'),
+    path(
+        "go-to-index/",
+        RedirectView.as_view(pattern_name="blog:cbv-index"),
+        name="go-to-index",
+    ),
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView, RedirectView
 from .models import Post
 # Create your views here.
 
 def indexView(request):
+    """
+    a function based view to show index page
+    """
     return render(request,"index.html")
 
 class IndexView(TemplateView):
+    """
+    a class based view to show index page
+    """
     template_name = "index.html"
 
     def get_context_data(self, **kwargs):
