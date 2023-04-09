a9fe6dd80612c56d75f4fa68912806bff789a13a
farzan-alaei <farzan.70@gmail.com>
2023-04-09 17:45:11 +0330

    add another redirectview in urls.py in blog app
    add path to urls for redirect url to send parameter with url with create class redirect to google

--- core/blog/views.py
+++ core/blog/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView, RedirectView
 from .models import Post
+from django.shortcuts import redirect, get_object_or_404
+
 # Create your views here.
 
+# Function Base view show a template
+'''
 def indexView(request):
     """
     a function based view to show index page
     """
     return render(request,"index.html")
+'''
 
 class IndexView(TemplateView):
     """
@@ -16,7 +21,23 @@ class IndexView(TemplateView):
     template_name = "index.html"
 
     def get_context_data(self, **kwargs):
-        contex = super().get_context_data(**kwargs)
-        contex["name"] = "ali"
-        contex["posts"] = Post.objects.all()
-        return contex
+        context = super().get_context_data(**kwargs)
+        context["name"] = "ali"
+        context["posts"] = Post.objects.all()
+        return context
+    
+
+
+
+''' FBV for redirect
+def redirectToGoogle(request):
+    return redirect("https://google.com")
+'''
+
+class RedirectToGoogle(RedirectView):
+    url = "https://google.com"
+
+    def get_redirect_url(self, *args, **kwargs):
+        post = get_object_or_404(Post, pk=kwargs['pk'])
+        print(post)
+        return super().get_redirect_url(*args, **kwargs)
