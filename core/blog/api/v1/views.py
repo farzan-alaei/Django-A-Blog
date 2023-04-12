78d99cb2089cc82794751b4a8fa7f4a810182dd9
farzan-alaei <farzan.70@gmail.com>
2023-04-12 15:59:39 +0330

    add deleting method to postDetail func
    add deleting method to postDetail and status set for 20(no content)

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -7,9 +7,10 @@ from django.shortcuts import get_object_or_404
 
 
 data = {
-    "id": 1, 
+    "id": 1,
     "title": "hello",
 }
+
 
 @api_view(["GET", "POST"])
 def postList(request):
@@ -22,9 +23,9 @@ def postList(request):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
-        
 
-@api_view(["GET", "PUT"])
+
+@api_view(["GET", "PUT", "DELETE"])
 def postDetail(request, id):
     post = get_object_or_404(Post, pk=id, status=True)
     if request.method == "GET":
@@ -35,3 +36,6 @@ def postDetail(request, id):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    elif request.method == "DELETE":
+        post.delete()
+        return Response({"detail": "item removed successfully"}, status=status.HTTP_204_NO_CONTENT)
