25f3c5a28b969d989ec97edfecccf9330befa5b3
farzan-alaei <farzan.70@gmail.com>
2023-04-12 13:48:14 +0330

    add 404 status code in postDetail in views
    add get_object_or_404 to postDetail Function for handling object does not exist (404)

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -2,6 +2,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import PostSerializer
 from ...models import Post
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 
 data = {
@@ -16,6 +18,7 @@ def postList(request):
 
 @api_view()
 def postDetail(request, id):
-    post = Post.objects.get(pk=id)
+    post = get_object_or_404(Post, pk=id)
     serializer = PostSerializer(post)
     return Response(serializer.data)
+    
