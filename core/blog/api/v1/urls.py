7e9282ac20ea1054456bc93a46f97bd67e778e33
farzan-alaei <farzan.70@gmail.com>
2023-04-12 12:56:05 +0330

    add postDetal func in api views
    add postDetail func in api views for testing the func
    add path for viewing the postDetail func in url

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -4,7 +4,6 @@ app_name = "api-v1"
 
 
 urlpatterns = [
-
     path('post/', views.postList, name='post-list'),
-
+    path('post/<int:id>/', views.postDetail, name='post-detail'),
 ]
--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,6 +1,17 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+
+data = {
+    "id": 1, 
+    "title": "hello",
+}
+
 @api_view()
 def postList(request):
     return Response("ok")
+
+
+@api_view()
+def postDetail(request, id):
+    return Response(data)
