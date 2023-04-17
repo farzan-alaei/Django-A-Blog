c8f15a5f05dea93c2f1a97efe3dc0ea4d93cc92a
farzan-alaei <farzan.70@gmail.com>
2023-04-17 17:30:27 +0330

    add APIView to views in api folder
    add Apiview to views
    add path for showing the class (PostList)

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -5,26 +5,46 @@ from .serializers import PostSerializer
 from ...models import Post
 from rest_framework import status
 from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+
+# data = {
+#     "id": 1,
+#     "title": "hello",
+# }
 
 
-data = {
-    "id": 1,
-    "title": "hello",
-}
+# @api_view(["GET", "POST"])
+# @permission_classes([IsAuthenticated])
+# def postList(request):
+#     if request.method == "GET":
+#         posts = Post.objects.filter(status=True)
+#         serializer = PostSerializer(posts, many=True)
+#         return Response(serializer.data)
+#     elif request.method == "POST":
+#         serializer = PostSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
 
+class PostList(APIView):
+    '''getting a list of posts and creating new posts'''
 
-@api_view(["GET", "POST"])
-@permission_classes([IsAuthenticated])
-def postList(request):
-    if request.method == "GET":
+    def get(self, request):
+        '''retrieving a list of post   '''
         posts = Post.objects.filter(status=True)
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
-    elif request.method == "POST":
+    
+    def post(self, request):
+        '''creating a post with provided data'''
         serializer = PostSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+
+
+
+
 
 
 @api_view(["GET", "PUT", "DELETE"])
