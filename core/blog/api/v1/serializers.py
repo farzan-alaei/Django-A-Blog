93efc8bf3bc9f0524ad170b437a87d6bb2dbf236
farzan-alaei <farzan.70@gmail.com>
2023-04-29 18:40:36 +0330

    add get_abs_url to serializers
    add get_abs_url to serializers for showing full path in drf and view in front

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -9,11 +9,16 @@ from ...models import Post, Category
 class PostSerializer(serializers.ModelSerializer):
     snippet = serializers.ReadOnlyField(source='get_snippet')
     relative_url = serializers.URLField(source='get_absolute_api_url', read_only=True)
+    absolute_url = serializers.SerializerMethodField(method_name='get_abs_url')
 
     class Meta:
         model = Post
         fields = ["id", "author", "title", "content", "status", "snippet", 
-                  "relative_url" ,"created_date", "published_date"]
+                  "relative_url" , "absolute_url","created_date", "published_date"]
+
+    def get_abs_url(self, obj):
+        request = self.context.get('request')
+        return request.build_absolute_uri(obj.pk)
 
 
 class CategorySerializer(serializers.ModelSerializer):
