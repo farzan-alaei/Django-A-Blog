3e5ae334e13081d6d6c783d88f9aaf43f2cc0488
farzan-alaei <farzan.70@gmail.com>
2023-04-29 19:37:06 +0330

    add to_representation function to serializer
    add to_representation function to serializer for seprate get and post method for showing in front

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -6,23 +6,29 @@ from ...models import Post, Category
 #     title = serializers.CharField(max_length=255)
 
 
-class PostSerializer(serializers.ModelSerializer):
-    snippet = serializers.ReadOnlyField(source='get_snippet')
-    relative_url = serializers.URLField(source='get_absolute_api_url', read_only=True)
-    absolute_url = serializers.SerializerMethodField(method_name='get_abs_url')
-
-    class Meta:
-        model = Post
-        fields = ["id", "author", "title", "content", "status", "snippet", 
-                  "relative_url" , "absolute_url","created_date", "published_date"]
-
-    def get_abs_url(self, obj):
-        request = self.context.get('request')
-        return request.build_absolute_uri(obj.pk)
-
-
 class CategorySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Category
         fields = ['id' ,'name']
+
+
+class PostSerializer(serializers.ModelSerializer):
+    snippet = serializers.ReadOnlyField(source='get_snippet')
+    relative_url = serializers.URLField(source='get_absolute_api_url', read_only=True)
+    absolute_url = serializers.SerializerMethodField(method_name='get_abs_url')
+    
+    class Meta:
+        model = Post
+        fields = ["id", "author", "title", "content", "status", "snippet", "category",
+                  "relative_url" , "absolute_url","created_date", "published_date"]
+
+    def get_abs_url(self, obj):
+        request = self.context.get('request')
+        return request.build_absolute_uri(obj.pk)
+    
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep['category'] = CategorySerializer(instance.category).data
+        rep.pop('snippet', None)
+        return rep
